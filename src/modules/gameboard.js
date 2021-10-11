0dcc72e28bdc925e273f00c3afb0044da93ed930
David Stockdale <dstock3@gmail.com>
2021-10-11 15:52:42 -0500

    Created a spaceArray property within the createGrid function.

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,23 +1,25 @@
 import { Ship } from './ship'
 
 const Gameboard = () => {
-
     const letterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
     const space = (position) => {
         return { position }
     }
 
-    const createGrid = () => {
+    const createGrid = (() => {
+        const spaceArray = []
         for (let i = 0; i < letterArray.length; i++) {
             for (let y = 1; y < 10; i++) {
-                space(`${letterArray[i]} + y`)
+                let newSpace = space(`${letterArray[i] + y}`)
+                spaceArray.push(newSpace);
             };
         };
-    };
+    })();
 
     const placeShip = () => {
 
     }
 
+    return { createGrid, placeShip, spaceArray }
 }
