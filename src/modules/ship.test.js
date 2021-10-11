bab30f890e1346c272c379c043b9f93c4480ba57
David Stockdale <dstock3@gmail.com>
2021-10-11 09:40:36 -0500

    Created tests for ship object attributes.

--- src/modules/ship.test.js
+++ src/modules/ship.test.js
@@ -1,6 +1,6 @@
 import { shipMaker } from "./ship";
 
-const testShip = shipMaker(3, "no hits", "active");
+const testShip = shipMaker(3, [false, false, true], "active");
 
 test('shipMaker returns an object with appropriate attributes', () => {
     expect(testShip.length).not.toBeNull;
@@ -10,4 +10,12 @@ test('shipMaker returns an object with appropriate attributes', () => {
 
 test('shipMaker object length is a number', () => {
     expect(typeof testShip.length).toBe("number");
+});
+
+test('hitposition attribute is an array containing boolean values', () => {
+    let hits = testShip.hitposition
+    for (let i = 0; i < hits.length; i++) {
+        expect(typeof hits[i]).toBe("boolean");
+    }
+    
 });
