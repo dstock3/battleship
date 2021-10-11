2172f3db00483b9ff4f33830a99402a2fbd647b7
David Stockdale <dstock3@gmail.com>
2021-10-11 15:40:05 -0500

    Created the basic setup for a gameboard factory function.

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -3,7 +3,7 @@ const Ship = (length, hitCount, status) => {
         hitCount[position] = true;
     };
 
-    const isSunk = () => hitCount.every((i) => i === true);
+    const isSunk = () => hitCount.every((position) => position === true);
     
     return { length, hitCount, status, hit, isSunk }
 };
