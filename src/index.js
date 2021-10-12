2e274f36d22d296d15f5e586c115ff82fd314556
David Stockdale <dstock3@gmail.com>
2021-10-12 11:23:04 -0500

    Created the basic div elements for columns and spaces in the DOM.

--- src/index.js
+++ src/index.js
@@ -7,19 +7,32 @@ let playerBoard = document.createElement("div");
 playerBoard.classList.add("player-board");
 body.appendChild(playerBoard);
 
+//Columns
+for (let y = 0; y < newBoard.letterArray.length; y++) {
+    let column = document.createElement("div");
+    column.classList.add("col");
+    column.id = newBoard.letterArray[y];
+    playerBoard.appendChild(column);
+};
+
+//Spaces
+let spaceArray = [];
 for (let i = 0; i < newBoard.spaceArray.length; i++) {
     let spaceObj = newBoard.spaceArray[i];
     let newSpace = document.createElement("div");
     newSpace.classList.add("space");
     newSpace.id = spaceObj.coord;
+    spaceArray.push(newSpace);
+};
 
-    for (let y = 0; y < newBoard.letterArray.length; y++) {
-        if (newSpace.id.charAt(0) === newBoard.letterArray[y]) {
+let columns = Array.from(document.getElementsByClassName("col"))
 
-        }
-    }
-
-
-    playerBoard.appendChild(row);
+for (let x = 0; x < spaceArray.length; x++) {
+    let space = spaceArray[x]
+    for (let z = 0; z < columns.length; z++) {
+        if (space.id.charAt(0) === columns[z].id) {
+            columns[z].appendChild(space);
+        };
+    };
 };
 
