1fe2e8074ef7f4b09598547c2de068de529f8db1
David Stockdale <dstock3@gmail.com>
2021-10-12 11:00:51 -0500

    Added a nested for loop to create a column for each letter.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import './style.css'
-import { Ship, shipSet } from "./modules/ship";
+import { Gameboard } from "./modules/gameboard";
 
+const newBoard = Gameboard();
 const body = document.getElementsByTagName("body")[0];
-
-const newBoard = Gameboard()
+let playerBoard = document.createElement("div");
+playerBoard.classList.add("player-board");
+body.appendChild(playerBoard);
 
 for (let i = 0; i < newBoard.spaceArray.length; i++) {
     let spaceObj = newBoard.spaceArray[i];
     let newSpace = document.createElement("div");
     newSpace.classList.add("space");
     newSpace.id = spaceObj.coord;
-}
+
+    for (let y = 0; y < newBoard.letterArray.length; y++) {
+        if (newSpace.id.charAt(0) === newBoard.letterArray[y]) {
+
+        }
+    }
+
+
+    playerBoard.appendChild(row);
+};
 
