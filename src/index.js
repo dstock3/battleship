69b1fee0385664ccacea1777d0eea280f99c8bc0
David Stockdale <dstock3@gmail.com>
2021-10-12 10:54:02 -0500

    Created the initial setup for a gameboard display.:

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './style.css'
 import { Ship, shipSet } from "./modules/ship";
 
+const body = document.getElementsByTagName("body")[0];
 
+const newBoard = Gameboard()
+
+for (let i = 0; i < newBoard.spaceArray.length; i++) {
+    let spaceObj = newBoard.spaceArray[i];
+    let newSpace = document.createElement("div");
+    newSpace.classList.add("space");
+    newSpace.id = spaceObj.coord;
+}
 
