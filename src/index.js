bd1464cc923aa75ae08c182b47a5838ea163b423
David Stockdale <dstock3@gmail.com>
2021-10-12 11:26:38 -0500

    Wrapped board elements within an IIFE.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
 import './style.css'
 import { Gameboard } from "./modules/gameboard";
 
-const newBoard = Gameboard();
 const body = document.getElementsByTagName("body")[0];
-let playerBoard = document.createElement("div");
-playerBoard.classList.add("player-board");
-body.appendChild(playerBoard);
 
-//Columns
-for (let y = 0; y < newBoard.letterArray.length; y++) {
-    let column = document.createElement("div");
-    column.classList.add("col");
-    column.id = newBoard.letterArray[y];
-    playerBoard.appendChild(column);
-};
+const boardGen = (() => {
+    const newBoard = Gameboard();
+    let playerBoard = document.createElement("div");
+    playerBoard.classList.add("player-board");
+    body.appendChild(playerBoard);
 
-//Spaces
-let spaceArray = [];
-for (let i = 0; i < newBoard.spaceArray.length; i++) {
-    let spaceObj = newBoard.spaceArray[i];
-    let newSpace = document.createElement("div");
-    newSpace.classList.add("space");
-    newSpace.id = spaceObj.coord;
-    spaceArray.push(newSpace);
-};
+    //Columns
+    for (let y = 0; y < newBoard.letterArray.length; y++) {
+        let column = document.createElement("div");
+        column.classList.add("col");
+        column.id = newBoard.letterArray[y];
+        playerBoard.appendChild(column);
+    };
 
-let columns = Array.from(document.getElementsByClassName("col"))
+    //Spaces
+    let spaceArray = [];
+    for (let i = 0; i < newBoard.spaceArray.length; i++) {
+        let spaceObj = newBoard.spaceArray[i];
+        let newSpace = document.createElement("div");
+        newSpace.classList.add("space");
+        newSpace.id = spaceObj.coord;
+        spaceArray.push(newSpace);
+    };
 
-for (let x = 0; x < spaceArray.length; x++) {
-    let space = spaceArray[x]
-    for (let z = 0; z < columns.length; z++) {
-        if (space.id.charAt(0) === columns[z].id) {
-            columns[z].appendChild(space);
+    let columns = Array.from(document.getElementsByClassName("col"))
+
+    for (let x = 0; x < spaceArray.length; x++) {
+        let space = spaceArray[x]
+        for (let z = 0; z < columns.length; z++) {
+            if (space.id.charAt(0) === columns[z].id) {
+                columns[z].appendChild(space);
+            };
         };
     };
-};
+})();
 
