fa57de268c70aa3c9103d75161885df3897c3056
David Stockdale <dstock3@gmail.com>
2021-11-15 15:03:09 -0600

    Added cursor style to enemy positions on gameboard. Added new parameters to yourMove to account for previous hit.

--- src/modules/DOM/events.js
+++ src/modules/DOM/events.js
@@ -17,7 +17,6 @@ const registerHit = (playerBoard, hitArray) => {
 };
 
 const shipDestroyed = (messageBox, shipList, coords, player) => {
-    console.log(shipList, coords, player)
     for (let i = 0; i < shipList.length; i++) {
         let ship = shipList[i];
         
@@ -26,79 +25,92 @@ const shipDestroyed = (messageBox, shipList, coords, player) => {
                 for (let x = 0; x < ship.base.hitCount.length; x++) {
                     if (!ship.base.hitCount[x]) {
                         ship.base.hitCount[x] = true;
-
+                        
                         if (ship.base.isSunk()) {
                             if (player === "player") {
                                 messageBox.textContent = `You have sunk the enemy's ${ship.type}!`;
                                 return true
-                            } else {
+                            } else if (player === "enemy") {
+                                console.log("Enemy ship base: " + ship.base)
                                 messageBox.textContent = `The enemy has sunk your ${ship.type}!`;
                                 return true
                             };
                         } else {
                             return false
                         };
+                        break   
                     };
                 };
+                break
             };
         };
     };
 };
 
-const yourMove = (enemyPositions, playerBoard, playerShipList, enemy, newPlayer, registerHit, playerHits, enemyHits) => {
+const yourMove = (enemyPositions, playerBoard, playerShipList, enemy, newPlayer, registerHit, playerHits, enemyHits, enemyTarget, previousCoord) => {
     let enemyBoard = enemyPositions.enemyBoard
     let messageBox = document.querySelector(".message-box");
 
-    function enemyThought(hit, result) {
-        if ((hit) && (!result)) {
-            messageBox.textContent = "It's a Direct Hit!"
-        } else if ((!result) || (!hit)) {
-            messageBox.textContent = "Shhh! The Enemy is Trying to Think."
-        };
-        
-        let delayBase = Math.random() * (3 - 1) + 1;
-        let delay = delayBase * 1000;
-        setTimeout(enemyIsThinking, delay);
-    }
+    for (let i = 0; i < enemyBoard.spaceElements.length; i++) {
+        enemyBoard.spaceElements[i].style.cursor = "crosshair"
+    };
 
     for (let i = 0; i < enemyBoard.spaceElements.length; i++) {
         function performMove() {
-            
             let coords = enemyBoard.spaceElements[i].id.replace(enemyBoard.spaceElements[i].id.charAt(0),'');;
             let playerHitArray = newPlayer.move(coords);
             let potentialPlayerHit = registerHit(enemyBoard, playerHitArray);
             playerHits += potentialPlayerHit;
             let result = shipDestroyed(messageBox, enemyPositions.enemyShipList, coords, "player");
-
-            enemyThought(playerHitArray[0], result);
+            enemyThought(playerHitArray[0], result, enemyTarget);
+            enemyBoard.spaceElements[i].removeEventListener("click", performMove);
+            enemyBoard.spaceElements[i].style.cursor = "default"
         };
         enemyBoard.spaceElements[i].addEventListener("click", performMove);
     };
 
-    function enemyIsThinking() {
-        let enemyHitArray =  enemy.randomMove();
-        if (enemyHitArray[0]) { 
-            messageBox.textContent = "You've Been Hit! Your Move." 
+    function enemyThought(isHit, result, enemyTarget) {
+        if ((isHit) && (!result)) {
+            messageBox.textContent = "It's a Direct Hit!"
+        } else if ((!result) || (!isHit)) {
+            messageBox.textContent = "Shhh! The Enemy is Trying to Think."
+        };
+        
+        let delayBase = Math.random() * (3 - 1) + 1;
+        let delay = delayBase * 1000;
+
+        const enemyHone = () => {
+            enemyIsThinking(enemyTarget, previousCoord)
+        }
+        setTimeout(enemyHone, delay);
+    }
+
+    function enemyIsThinking(enemyTarget, previousCoord) {
+        let enemyHitArray
+        if (enemyTarget) {
+            enemyHitArray =  enemy.educatedGuess(previousCoord);
         } else {
-            messageBox.textContent = "Your Move." 
+            enemyHitArray =  enemy.randomMove();
         };
 
+        let isHit = enemyHitArray[0];
         let potentialEnemyHit = registerHit(playerBoard, enemyHitArray);
         enemyHits += potentialEnemyHit
-        shipDestroyed(messageBox, playerShipList, enemyHitArray[2], "enemy");
+        let result = shipDestroyed(messageBox, playerShipList.coords, enemyHitArray[2], "enemy");
+        console.log("Enemy result: " + result);
+
+        if ((!result) && (isHit)) { 
+            messageBox.textContent = "You've Been Hit! Your Move." 
+        } else if ((!result) || (!isHit)) {
+            messageBox.textContent = "Your Move." 
+        }
         
         console.log("Player Hits: " + playerHits)
         console.log("Enemy Hits: " + enemyHits)
 
         if ((playerHits === 17) || (enemyHits === 17)) {
             console.log("game over")
-        }
-
-        if (enemyHitArray[0]) {
-            //removeListener(enemyBoard, performMove)
-            //nextMove(enemyPositions, playerBoard, playerShipList, enemy, newPlayer, registerHit, enemyHitArray, playerHits, enemyHits);  
         };
-        
     };
 };
 
